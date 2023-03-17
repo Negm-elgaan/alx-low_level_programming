b9b49e0a0918cb786ab17b293bb269b67d67e3dc
Negm-elgaan <ohossam962@gmail.com>
2023-03-17 16:47:05 -0700

    ./2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - mian function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+    char ch = 'a';
+	while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+	putchar('\n');
+	return (0);
+
+}
