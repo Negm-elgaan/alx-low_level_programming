5c3421f20380fe9c8846009bd0078162ffa6778e
Negm-elgaan <ohossam962@gmail.com>
2023-04-19 17:34:29 -0700

    kgggg

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+* print_name - function that stores pointer function
+*
+* @name:parameter to be changed
+*
+* @f:parameter to be changed
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
